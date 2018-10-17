f09ee89ff7c1398d58ab005bb0c10758243f3f85
Karl Gustafsson <karl.gustafsson@live.com>
2018-10-17 18:14:37 +0200

    muaw

--- src/com/knopp10000/ImageViewer/Main.java
+++ src/com/knopp10000/ImageViewer/Main.java
@@ -5,18 +5,16 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ItemListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
-import java.util.Scanner;
 
 public class Main {
     private JPanel panel1;
     private JButton getPictureButton;
     private JPanel PictureLabel;
-    BufferedImage screenImage;
+    private static final String defImage = "https://i.ytimg.com/vi/wcIQQkgPFBM/hqdefault.jpg";
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Main");
@@ -24,50 +22,51 @@ public class Main {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
-        //Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         frame.setBounds(0,0, 600, 600);
 
+        Main desu = null;
         if (args.length < 1) // by default program will load AnyExample logo
-            new Main("https://i.ytimg.com/vi/wcIQQkgPFBM/hqdefault.jpg");
-        else
-            new Main(args[0]);
+        {
+            //Default
+            desu = new Main(defImage);
+            frame.setContentPane(desu.panel1);
+        }else {
+            desu = new Main(args[0]);
+            frame.setContentPane(desu.panel1);
+        }
     }
 
     public Main(String source){
-
         // loading image
-        try {
-            if (source.startsWith("http://") || source.startsWith("https://") ) // http:// URL was specified
-            {
-                screenImage = ImageIO.read(new URL("https://i.ytimg.com/vi/wcIQQkgPFBM/hqdefault.jpg"));
-                System.out.print(source);
-            }else if (source == ""){
-                System.out.print("Hdasdasd");
-            }else {
-                screenImage = ImageIO.read(new File(source)); // otherwise - file
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
 
         getPictureButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
+                BufferedImage screenImage = null;
+                
+                try {
+                    if (source.startsWith("http://") || source.startsWith("https://") ) // http:// URL was specified
+                    {
+                        screenImage = ImageIO.read(new URL(source));
+                        System.out.print(source);
 
-                addPicture();
+                    }else if (source == ""){
+                        System.out.print("Erroooororororororor ");
+                    }else {
+                        screenImage = ImageIO.read(new File(source)); // otherwise - file
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                addPicture(screenImage);
             }
         });
     }
 
 
 
-    public void addPicture(){
-        System.out.print("sdsda");
-
+    private void addPicture(BufferedImage screenImage){
         try {
-            //BufferedImage myPicture = screenImage;
             JLabel picLabel = new JLabel(new ImageIcon(screenImage));
             PictureLabel.add(picLabel, BorderLayout.CENTER);
             PictureLabel.revalidate();
@@ -75,10 +74,5 @@ public class Main {
         }catch (NullPointerException e){
             e.printStackTrace();
         }
-    }
-
-
-    private void createUIComponents() {
-        // TODO: place custom component creation code here
     }
 }
