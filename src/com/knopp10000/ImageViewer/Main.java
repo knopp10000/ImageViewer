343a63e1bbcb008e3ed5572569c6d42677b15a27
Karl Gustafsson <karl.gustafsson@live.com>
2020-02-01 19:56:04 +0100

    Removed Main.form and shit does not work

--- src/com/knopp10000/ImageViewer/Main.java
+++ src/com/knopp10000/ImageViewer/Main.java
@@ -2,35 +2,37 @@ package com.knopp10000.ImageViewer;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
-import javax.swing.plaf.IconUIResource;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
-import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
 public class Main {
-    private JPanel panel1;
-    private JPanel PictureLabel;
+    private JFrame frame;
+    private JPanel mainPanel;
+    private JPanel picturePanel;
+    JLabel picLabel;
     private static final String defImage = "https://cdn.pixabay.com/photo/2017/03/02/09/26/question-mark-2110767_960_720.jpg";
     private static final String largeDefImage = "http://s1.picswalls.com/wallpapers/2015/09/20/2015-wallpaper_111525594_269.jpg";
 
     public static void main(String[] args) {
-        if (args.length < 1) // by default program will load AnyExample logo
-        {
+        if (args.length == 0){ // by default program will load An Example logo
             new Main().run(largeDefImage);
-        }else {
+        }else if(args.length == 1){
             new Main().run(args[0]);
+        }else{
+            throw new Error("Too many arguments. Only use 1.");
         }
      }
 
     public void run(String source){
-        JFrame frame = new JFrame("Epic ImageViewer");
+        frame = new JFrame("Epic ImageViewer");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setIcons(frame);
 
         /*if (isFullscreen()){
             frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
@@ -38,6 +40,45 @@ public class Main {
             frame.pack();
         }*/
 
+
+        picturePanel = new JPanel();
+        frame.add(picturePanel, BorderLayout.CENTER);
+
+        BufferedImage placeholder;
+        try {
+            placeholder = ImageIO.read(new File("/images/placeholder.png"));
+            picLabel = new JLabel(new ImageIcon(placeholder));
+        } catch (IOException e) {            e.printStackTrace();
+        }
+        picturePanel.add(picLabel, BorderLayout.CENTER);
+
+
+
+
+        BufferedImage image;
+        try {
+            if (source.startsWith("http://") || source.startsWith("https://") ) // http:// URL was specified
+            {
+                image = ImageIO.read(new URL(source));
+                System.out.println("Image source: " + source);
+                addPicture(resize(image));
+
+            }else if(source.isEmpty()){
+                throw new Exception("Empty Main call");
+            }else {
+                image = ImageIO.read(new File(source)); // otherwise - file
+                addPicture(resize(image));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        frame.pack();
+        frame.setVisible(true);
+
+    }
+
+    private void setIcons(JFrame frame) {
         BufferedImage img = null;
         try {
             img = ImageIO.read(getClass().getResource("/images/ImageViewerIcon.png"));
@@ -46,38 +87,13 @@ public class Main {
         }
         List<BufferedImage> icons = new LinkedList<>(Arrays.asList(img));
         frame.setIconImages(icons);
-
-        frame.setVisible(true);
-        // loading image
-        BufferedImage screenImage = null;
-        try {
-            if (source.startsWith("http://") || source.startsWith("https://") ) // http:// URL was specified
-            {
-                screenImage = ImageIO.read(new URL(source));
-                System.out.println("Source pic:" + source);
-                addPicture(resize(screenImage));
-
-            }else if (source.isEmpty()){
-                throw new Exception("Empty Main call");
-            }else {
-                screenImage = ImageIO.read(new File(source)); // otherwise - file
-                addPicture(resize(screenImage));
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        frame.pack();
-
-
     }
 
     private Image resize(BufferedImage source){
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        if(screenSize.getHeight() < source.getHeight() || screenSize.getWidth() < source.getWidth()) {
-            Image resizedImage = source.getScaledInstance((int)screenSize.getWidth(), (int)screenSize.getHeight(), Image.SCALE_SMOOTH);
-            return resizedImage;
-        }
+//        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+//        if(screenSize.getHeight() < source.getHeight() || screenSize.getWidth() < source.getWidth()) {
+//            return source.getScaledInstance((int)screenSize.getWidth(), (int)screenSize.getHeight(), Image.SCALE_SMOOTH);
+//        }
         return source;
     }
 
@@ -90,11 +106,10 @@ public class Main {
         }
     }*/
 
-    private void addPicture(Image screenImage){
+    private void addPicture(Image image){
         try {
-            JLabel picLabel = new JLabel(new ImageIcon(screenImage));
-            PictureLabel.add(picLabel, BorderLayout.CENTER);
-            PictureLabel.revalidate();
+            picLabel = new JLabel(new ImageIcon(image));
+            picturePanel.repaint();
 
         }catch (NullPointerException e){
             e.printStackTrace();
